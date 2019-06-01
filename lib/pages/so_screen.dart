6c6272f7b294c9f6495389436b489c8453ac937a
terryBaz <et_bazine@esi.dz>
2019-06-01 10:41:12 +0100

    github questions listing

--- lib/pages/so_screen.dart
+++ lib/pages/so_screen.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
 
-//import 'package:webview_flutter/webview_flutter.dart';
 import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 
 final mainUrl =
